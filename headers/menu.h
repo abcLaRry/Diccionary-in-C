97650ff51d5b5d9e0e0c9bd88e63d0fcf699b926
Dario Norrigan <104734535+abcLaRry@users.noreply.github.com>
2024-07-12 10:54:14 -0600

    Create menu.h

---
+++ headers/menu.h
@@ -0,0 +1,15 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+
+/*
+    Function headers, only one has parameters that are dependent on the required functionality, details in "trees.c"
+*/
+void clean();
+void inicialize();
+void addWord();
+void delWord();
+void displayAll();
+void showDetails();
+void close();
+
+#endif // MENU_H_INCLUDED
