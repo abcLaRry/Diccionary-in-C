ec90f6d8204e8112645d0d6911d20e71f8e38013
Dario Norrigan <104734535+abcLaRry@users.noreply.github.com>
2024-07-15 15:33:28 -0600

    Update structures.h

--- headers/structures.h
+++ headers/structures.h
@@ -1,13 +1,12 @@
 #ifndef STRUCTURES_H_INCLUDED
 #define STRUCTURES_H_INCLUDED
 #define QLETTERS 26 // This definition is the quantity of letters that exist in english alphabet
-
 /*
     Definition of a tree node (it is contained in Letter_t)
 */
 typedef struct _nodeT {
     char *word;
-    struct _noteT *right, *left;
+    struct _nodeT *right, *left;
 } Node_t;
 
 /*
@@ -20,6 +19,12 @@ typedef struct _node {
 } Letter_t;
 
 int checkInput(char *check);
-void insertWord(char *word);
+Node_t* isInTree(Node_t* root, char *word);
+Node_t* insertNode(Node_t* root, char *word);
+Node_t* deleteNode(Node_t* root, char *word);
+void printTree(Node_t* root);
+int countNodes(Node_t* root, char* word);
+void printRepeated(Node_t* root);
+void freeAllNodes(Node_t* root);
 
 #endif // STRUCTURES_H_INCLUDED
